acb9aecb7e9e6afcf39ed32fed5ca8f84705a0ae
Dan Buda <danbuda@gmail.com>
2018-10-15 16:01:02 -0500

    start work on logic to find exact dates of holidays

--- src/js/index.js
+++ src/js/index.js
@@ -157,3 +157,87 @@ calendarGrid.innerHTML = `This month has ${monthDays} days and starts on a ${day
 // Have specific messages for certain days (like my birthday) and probably set those
 // with if statements before rendering the message. And if there's no specific message for that day,
 // maybe just randomly choose form an array of options
+
+// Sandbox Code for Functions to be able to render any date
+
+// Temp Data
+const data = [
+  {
+    name: 'Christmas Day',
+    type: 'holiday',
+    dateType: 'static',
+    month: 11,
+    date: 25, // I don't need the month because it's in a separate key above
+  },
+  {
+    name: 'Martin Luther King, Jr. Day',
+    type: 'holiday',
+    dateType: 'flex',
+    month: 0,
+    date: {
+      nth: 3, // 3rd
+      weekday: 1, // Monday
+    },
+  },
+  {
+    name: "Dan Buda's Birthday",
+    type: 'birthday',
+    dateType: 'static',
+    month: 3,
+    date: 14,
+  },
+  {
+    name: 'Bill Filing Deadline',
+    type: 'legislative',
+    dateType: 'session',
+    month: 2,
+    date: 60, // 60th day of session
+  },
+];
+
+function filterData(data) {
+  const thisMonth = dateFns.getMonth(new Date());
+  console.log('thisMonth: ', thisMonth);
+
+  const filteredData = data
+    .filter(date => date.month === 2)
+    // Which date type are we lookg at when we loop over the eventual filtered array with
+    // only dates from the current month?
+
+    // This isn't going to work because it's not returning anything
+    .map(item => {
+      if (item.dateType === 'static') {
+        getStaticDate(item.month, item.date);
+      } else if (item.dateType === 'flex') {
+        getFlexDate(item.month, item.date.nth, item.date.weekday);
+      } else if (item.dateType === 'session') {
+        getSessionDate(sessionStartDate, item.date - 1);
+      }
+    });
+  console.log('filteredData: ', filteredData);
+}
+
+// Each of the 3 "get" functions will probably only run after an if statement determining
+// which type of date the current item is. So first we'd check to see if it's a "session"
+// type then we'd run it thru the getSessionDate function, so we will already know which
+// array item we're working with
+
+function getStaticDate(month, date) {}
+
+function getFlexDate(month, nth, day) {}
+
+function getSessionDate(start, days) {
+  console.log(dateFns.addDays(start, days));
+}
+
+filterData(data);
+
+// getSessionDate(sessionStartDate, 60 - 1); // this will probably look like (sessionStartDate, date - 1)
+
+// What needs to happen in the filterData() function above is to choose only the dates in
+// the data array that are in the current month (or the month that was clicked to by
+// forward or back arrow). Once all those dates are pulled out, I need the actual date
+// of the holiday/etc so that will come from one of the functions depending on the dateType
+// then once I have the date (I only need the day of the month at this point), I can use that
+// along with the name of the holiday/etc, and the item's type (to give it class and style
+// it in the DOM) to render to the screen
