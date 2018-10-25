e773648aa02ebc24b3fb6baf1ab92724a256ccc3
Dan Buda <danbuda@gmail.com>
2018-10-25 11:23:06 -0500

    clean up JS code/comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,11 @@
-// Grab elements needed for JavaScript operations
 const clockMessage = document.querySelector('.clock__message');
 const time = document.querySelector('.clock__time');
-// const calendar = document.querySelector('.calendar');
 const calendarTitle = document.querySelector('.cal__title');
 const calendarGrid = document.querySelector('.cal__grid');
 const leftBtn = document.querySelector('.cal__btn--left');
 const rightBtn = document.querySelector('.cal__btn--right');
 const calendarMobile = document.querySelector('.cal__mobile');
 
-// Add event listeners to back/forward month buttons
 leftBtn.addEventListener('click', () => {
   renderMonth(-1);
 });
@@ -24,10 +21,6 @@ let duringSession;
 let currentYear = dateFns.getYear(new Date());
 let currentMonth = dateFns.getMonth(new Date());
 
-// Determine next session start and next sine die
-// You'll need them for any calculation you need to do, so just
-// figure them out at the start and set them to a const
-
 // **************************************************************
 // Start of Countdown Clock Code
 
@@ -37,10 +30,9 @@ if (currentYear % 2 === 0) {
 } else {
   sessionYear = currentYear;
 }
+
 // Find the start of the sessionYear
-
 let januaryOne = dateFns.startOfYear(new Date(sessionYear, 0, 1));
-
 let sessionStartDate = januaryOne;
 
 // If year does not start on a Tuesday, keep adding a day at a time and checking, then add 7 days for the 2nd Tuesday
@@ -49,12 +41,10 @@ while (!dateFns.isTuesday(sessionStartDate)) {
 }
 sessionStartDate = dateFns.addDays(sessionStartDate, 7);
 
-// So when is Sine Die?
+// Date of Sine Die
 sineDieDate = dateFns.addDays(sessionStartDate, 139);
 
-// Next, figure out if we are during session and need to calculate time until Sine Die,
-// or before session and need to calculate time until session starts
-
+// Calculate if clock should countdown until end of session or until session starts
 function countdown() {
   let now = new Date();
   let end;
@@ -62,7 +52,7 @@ function countdown() {
     dateFns.isBefore(sessionStartDate, now) &&
     dateFns.isAfter(sineDieDate, now)
   ) {
-    // Need duringSession only for determining UI for 'time until sine die' or 'time until session'
+    // duringSession determines which message shows above countdown clock
     duringSession = true;
     end = sineDieDate;
   } else {
@@ -72,17 +62,14 @@ function countdown() {
 
   // Calculate time left until (session start, sine die)
   const daysLeft = -dateFns.differenceInDays(now, end);
-
   const hoursLeft = -dateFns.differenceInHours(
     dateFns.addDays(now, daysLeft),
     end
   );
-
   const minutesLeft = -dateFns.differenceInMinutes(
     dateFns.addHours(dateFns.addDays(now, daysLeft), hoursLeft),
     end
   );
-
   const secondsLeft = -dateFns.differenceInSeconds(
     dateFns.addMinutes(
       dateFns.addHours(dateFns.addDays(now, daysLeft), hoursLeft),
@@ -120,13 +107,9 @@ function countdown() {
 `;
 }
 
-// What happens when everything is at 0? Need to stop it running? Or someone automatically
-// switch it over to until sine die? I think it might already do this
-
-// Need to be able to test what happens when everything hits 0
-
 setInterval(countdown, 1000);
 countdown();
+
 // ************************************************************
 // Start of Calendar Code
 
@@ -154,24 +137,6 @@ const days = [
   'Friday',
   'Saturday',
 ];
-// Show a list of important session dates, holidays on Patsty's calendar, and my birthday
-// Color code for holiday, session event
-// For mobile, definitely just a list of events, not an actual calendar
-// left/right arrows for changing the month shown
-
-// meybe an actual calendar-looking render for destktop size?
-
-// Need to populate days of the month (probably with dateFns)
-// Need to merge month dates with holidays and session days, given that they won't always
-// fall on the same date
-
-// Start message of the day code
-
-// Have specific messages for certain days (like my birthday) and probably set those
-// with if statements before rendering the message. And if there's no specific message for that day,
-// maybe just randomly choose form an array of options
-
-// Sandbox Code for Functions to be able to render any date
 
 // Main Dataset (Rosh Hashanah, Yom Kippur, Easter & Good Friday are only good thru 2020 dates)
 const data = [
@@ -180,7 +145,7 @@ const data = [
     type: 'holiday',
     dateType: 'static',
     month: 11,
-    date: 25, // I don't need the month because it's in a separate key above
+    date: 25,
   },
   {
     name: 'Day After Christmas',
@@ -218,21 +183,21 @@ const data = [
   },
   {
     name: "Dan Buda's Birthday",
-    type: 'birthday',
+    type: 'other',
     dateType: 'static',
     month: 2,
     date: 14,
   },
   {
     name: "Harry Potter's Birthday",
-    type: 'birthday',
+    type: 'other',
     dateType: 'static',
     month: 6,
     date: 31,
   },
   {
     name: 'Original Star Wars premier in 1977',
-    type: 'birthday',
+    type: 'other',
     dateType: 'static',
     month: 4,
     date: 25,
@@ -437,6 +402,7 @@ const data = [
   },
 ];
 
+// Legislative dates
 const sessionData = [
   {
     name: 'Bill Filing Deadline',
@@ -562,30 +528,11 @@ const sessionData = [
   },
 ];
 
-// So maybe the overall function is:
 function renderMonth(change) {
-  // Everything goes in here
-
-  // NOTE: Should I just run the renderMonth function on page load and use a change
-  // value of 0 and check for that in the function? Then I can just do nothing to currentMonth
-  // and currentYear and run the same function as if a button is clicked.
-  // YES - DO THIS!
-
-  // Check what the current month and year are for the chosen calendar month, taking in
-  // to account if someone clicks from Dec to Jan you need to +1 the year and opposite
-  // for going back from Jan to Dec
-  // Test against the variable currentYear which will be updated everytime it needs to be
-  // So if you're in October 2018 (which would auto load at this point) and hit the right
-  // arrow, you would check to see if year changed (it doesn't) and you know month needs
-  // to go +1. So currentYear is 2018, current month is 9 but needs to change to 10 before
-  // running the function to render the holidays for November 2018
-  // You can do if change = +1 AND currentMonth is 11, then add +1 to year
-  // if change = -1 AND current month is 0, then -1 to year
-
   // First check to see if this is the initial run of the function on page load with
   // change = 0
   if (change !== 0) {
-    // The rest of this is setting up what the month and year are that are needed to filer
+    // The rest of this is setting up what the month and year are that are needed to filter
     // the holidays to render based on the button that was clicked (forward or backward a month)
     // If forward button clicked
     if (change === 1) {
@@ -612,19 +559,16 @@ function renderMonth(change) {
     }
   }
 
-  // show current month & year in calendar title
+  // Show current month & year in calendar title
   calendarTitle.textContent = `${months[currentMonth]} ${currentYear}`;
 
   // How many days in the current month?
   const monthDays = dateFns.getDaysInMonth(new Date(currentYear, currentMonth));
-
   // // What's the first day of the month?
   const firstDay = dateFns.startOfMonth(new Date(currentYear, currentMonth));
-
   const firstDayNum = dateFns.getDay(firstDay);
-  // const dayOfWeek = days[dateFns.getDay(new Date(firstDay))];
 
-  // Render the calendar into the calendar grid
+  // Empty arrays to push calendar data into
   let calendarInfo = [];
   let calendarInfoMobile = [];
 
@@ -639,11 +583,9 @@ function renderMonth(change) {
     let newDate;
 
     if (date.dateType === 'flex') {
-      // Figure out for flex date
-      // start by asking if the item's date.weekday is the first day of the month
-      // if so, start the next calculation
-      // if not, keep adding 1 until they match up
-
+      // Is item's date.weekday the first day of the month?
+      // If so, start the next calculation
+      // If not, keep adding 1 until they match up
       let dayCalc = date.date.weekday;
 
       let counter = 0;
@@ -655,7 +597,7 @@ function renderMonth(change) {
           dayCalc = --dayCalc;
         }
       }
-      // after matching up weekday days, next calc here
+      // After matching up weekday days, next calc here
       newDate = 1 + (date.date.nth - 1) * 7 + counter;
       // Only include general election if it's the correct year
     } else if (date.dateType === 'general' && currentYear % 2 === 0) {
@@ -664,7 +606,6 @@ function renderMonth(change) {
         monthStart = dateFns.addDays(monthStart, 1);
       }
       monthStart = dateFns.addDays(monthStart, 1);
-
       newDate = dateFns.getDate(new Date(monthStart));
     } else if (date.dateType === 'firstDay' && currentYear % 2 === 0) {
       let monthStart = dateFns.startOfMonth(new Date(currentYear, date.month));
@@ -705,7 +646,6 @@ function renderMonth(change) {
 
     // Calculate the month/date of session calendar dates
     const finalSessionData = sessionData.map(item => {
-      // console.log('sessionStartDate: ', sessionStartDate);
       const newDay = dateFns.addDays(
         new Date(calSessionStartDate),
         item.sessionDay - 1
@@ -728,36 +668,35 @@ function renderMonth(change) {
   }
 
   // *********************************************
+  // Populate Calendar
 
-  // Push 1st calendar block into array with class to denote is as first for starting position
-  // in grid
+  // Push 1st calendar block into array with style to put it into 1st day of month
   calendarInfo.push(
-    `<div class="day-block day-one" style="grid-column-start: ${firstDayNum +
-      1};"><div class="day-date">${1}</div>`
+    `<div class="cal__block" style="grid-column-start: ${firstDayNum +
+      1};"><div class="cal__date">${1}</div>`
   );
 
+  // Check if any calendar items in the final dataset occur on the 1st of the month
   finalData.forEach(item => {
     if (item.date === 1) {
       calendarInfo.push(
-        `<div class="calendar-item ${item.type}">${item.name}</div>`
+        `<div class="cal__item ${item.type}">${item.name}</div>`
       );
       calendarInfoMobile.push(
-        `<div class="calendar__item--mobile ${item.type}">${item.date}st ${
+        `<div class="cal__item--mobile ${item.type}">${item.date}st ${
           item.name
         }</div>`
       );
     }
   });
 
+  // Close div tag of 1st day of month
   calendarInfo.push(`</div>`);
 
-  // For adding events, if any events in the already filtered array of events (where
-  // I filter only for those in the month being rendered), match the day I'm pushing
-  // to the array, add it into the "day-block" class after the "day-date" div.
-
+  // Populate the rest of month's dates
   for (var i = 2; i < monthDays + 1; i++) {
     calendarInfo.push(
-      `<div class="day-block"><div class="day-date">${i}</div>`
+      `<div class="cal__block"><div class="cal__date">${i}</div>`
     );
 
     finalData.forEach(item => {
@@ -773,12 +712,12 @@ function renderMonth(change) {
       }
       if (item.date === i) {
         calendarInfo.push(`
-            <div class="calendar-item ${item.type}">${item.name}</div>
+            <div class="cal__item ${item.type}">${item.name}</div>
           `);
         calendarInfoMobile.push(
-          `<div class="calendar__item--mobile ${item.type}">${
-            item.date
-          }${suffix} ${item.name}</div>`
+          `<div class="cal__item--mobile ${item.type}">${item.date}${suffix} ${
+            item.name
+          }</div>`
         );
       }
     });
@@ -786,12 +725,10 @@ function renderMonth(change) {
     calendarInfo.push(`</div>`);
   }
 
-  // Use firstDay to set where the calendar for that month starts in the calendar-view
-  // version and fill in the grid wrapping until the last day of the month
+  // Render calendar(s)
   calendarGrid.innerHTML = calendarInfo.join('');
   calendarMobile.innerHTML = calendarInfoMobile.join('');
 }
 
-// Then that function gets called on page load and when someone wants to go
-// back/forward months using the arrows
+// Initial calendar render
 renderMonth(0);
