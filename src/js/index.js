fa6d12fccbb6a2587254d048a4a2dbba53511043
Dan Buda <danbuda@gmail.com>
2018-10-29 17:13:32 -0500

    countown function finished, tested, and added to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -13,10 +13,6 @@ rightBtn.addEventListener('click', () => {
   renderMonth(1);
 });
 
-let sessionYear;
-let sineDieDate;
-let duringSession;
-
 // Set variables for current month and year for initial calendar render
 let currentYear = dateFns.getYear(new Date());
 let currentMonth = dateFns.getMonth(new Date());
@@ -24,44 +20,103 @@ let currentMonth = dateFns.getMonth(new Date());
 // **************************************************************
 // Start of Countdown Clock Code
 
-// Determine if odd or even year then if even, add a year to currentYear
-if (currentYear % 2 === 0) {
-  sessionYear = currentYear + 1;
-  console.log(sessionYear);
-} else {
-  sessionYear = currentYear;
-  console.log(sessionYear);
-}
-
-// Find the start of the sessionYear
-let januaryOne = dateFns.startOfYear(new Date(sessionYear, 0, 1));
-let sessionStartDate = januaryOne;
-
-// If year does not start on a Tuesday, keep adding a day at a time and checking, then add 7 days for the 2nd Tuesday
-while (!dateFns.isTuesday(sessionStartDate)) {
-  sessionStartDate = dateFns.addDays(sessionStartDate, 1);
-}
-sessionStartDate = dateFns.addDays(sessionStartDate, 7);
-
-// Date of Sine Die
-sineDieDate = dateFns.addDays(sessionStartDate, 139);
-
-// Calculate if clock should countdown until end of session or until session starts
 function countdown() {
-  let now = new Date();
+  // Create variable to store what the year of session will be
+  let sessionYear;
+  // find the current year (2019, 0, 1 is only in there for testing - it should be empty)
+  const currentYear = dateFns.getYear(new Date());
+  // is "now" during session or not?
+  let duringSession;
+  // create variable to store the date that session starts
+  let sessionStartDate;
+
+  // store date for sine die
+  let sineDieDate;
+
+  // get info for right now and store in variable "now" (2019, 2, 15 is only in for testting - should be blank)
+  const now = new Date();
+  // console.log('now: ', now);
+  // create variable to store the end date for the countdown depending on when during the 2 year cycle you are
   let end;
-  if (
-    dateFns.isBefore(sessionStartDate, now) &&
-    dateFns.isAfter(sineDieDate, now)
-  ) {
-    // duringSession determines which message shows above countdown clock
-    duringSession = true;
-    end = sineDieDate;
-  } else {
-    duringSession = false;
-    // During interim, 'end' needs to be the NEXT sessionStartDate
-    end = sessionStartDate;
+  // console.log('currentYear: ', currentYear);
+
+  // function to get the 1st day of a legislative session
+  function getDate(year) {
+    let startDate = dateFns.startOfYear(new Date(year, 0, 1));
+    while (!dateFns.isTuesday(startDate)) {
+      startDate = dateFns.addDays(startDate, 1);
+    }
+    sessionStartDate = dateFns.addDays(startDate, 7);
+    sessionStartDate = dateFns.addHours(sessionStartDate, 12);
+    // console.log('sSD: ', sessionStartDate);
+    // sineDieDate = dateFns.addDays(sessionStartDate, 139);
+    // console.log('sessionStartDate: ', sessionStartDate);
+    // console.log('sineDieDate: ', sineDieDate);
   }
+
+  if (currentYear % 2 === 0) {
+    console.log('In an even year');
+    // In even year so need to get sessionStartDate for next year
+    // So session year has to be next year
+    sessionYear = currentYear + 1;
+    getDate(sessionYear);
+    // in this scenario, we're counting down to when session starts
+    end = sessionStartDate;
+    // this time period is not during session
+    duringSession = false;
+    // console.log('countdown end: ', end);
+    // console.log('duringSession: ', duringSession);
+  } else {
+    //You're in an odd year now
+    console.log('In an odd year');
+    sessionYear = currentYear;
+    // get session start date for the current year (it may or may not already have passed)
+    getDate(sessionYear);
+    // when is sine die of this year?
+    sineDieDate = dateFns.addDays(sessionStartDate, 139);
+    sineDieDate = dateFns.subHours(sineDieDate, 12);
+    // console.log(
+    //   'sessionStartDate when you get to odd year code: ',
+    //   sessionStartDate
+    // );
+    // console.log(
+    //   'sine die date at first getting to odd year code: ',
+    //   sineDieDate
+    // );
+    // console.log('sessionYear: ', sessionYear);
+    // If you're in session:
+    if (
+      dateFns.isBefore(sessionStartDate, now) &&
+      dateFns.isBefore(now, sineDieDate)
+    ) {
+      duringSession = true;
+      console.log('In the middle of session in an odd year');
+      end = sineDieDate;
+      // console.log('during session end: ', end);
+      // Not in session but during session year and before session start date
+    } else if (dateFns.isBefore(now, sessionStartDate)) {
+      duringSession = false;
+      end = sessionStartDate;
+      console.log('In an odd year before session starts');
+      // console.log('before session during session year end: ', end);
+      // Not in session but in session year and after sine die
+    } else {
+      console.log('In an odd year after session has ended');
+      const nextSessionYear = sessionYear + 2;
+      console.log('nSY: ', nextSessionYear);
+      getDate(nextSessionYear);
+      end = sessionStartDate;
+      // Not in session but after sine die
+      duringSession = false;
+      // end = next session start date 2 years away
+
+      // getDate(sessionYear + 2);
+      // end = sessionStartDate;
+      // console.log('after sine die end: ', end);
+    }
+  }
+  console.log('now: ', now);
+  console.log('end: ', end);
 
   // Calculate time left until (session start, sine die)
   const daysLeft = -dateFns.differenceInDays(now, end);
@@ -88,25 +143,25 @@ function countdown() {
   }
 
   time.innerHTML = `
-    <ul>
-      <li class="clock__time--numbers">${daysLeft}</li>
-      <li class="clock__time--numbers">:</li>
-      <li class="clock__time--numbers">${
-        hoursLeft < 10 ? '0' : ''
-      }${hoursLeft}</li>
-      <li class="clock__time--numbers">:</li>
-      <li class="clock__time--numbers">${
-        minutesLeft < 10 ? '0' : ''
-      }${minutesLeft}</li>
-      <li class="clock__time--numbers">:</li>
-      <li class="clock__time--numbers">${
-        secondsLeft < 10 ? '0' : ''
-      }${secondsLeft}</li>
-      <li class="clock__time--labels">Days</li>
-      <li class="clock__time--labels">Hours</li>
-      <li class="clock__time--labels">Minutes</li>
-      <li class="clock__time--labels">Seconds</li>
-    </ul>
+  <ul>
+    <li class="clock__time--numbers">${daysLeft}</li>
+    <li class="clock__time--numbers">:</li>
+    <li class="clock__time--numbers">${
+      hoursLeft < 10 ? '0' : ''
+    }${hoursLeft}</li>
+    <li class="clock__time--numbers">:</li>
+    <li class="clock__time--numbers">${
+      minutesLeft < 10 ? '0' : ''
+    }${minutesLeft}</li>
+    <li class="clock__time--numbers">:</li>
+    <li class="clock__time--numbers">${
+      secondsLeft < 10 ? '0' : ''
+    }${secondsLeft}</li>
+    <li class="clock__time--labels">Days</li>
+    <li class="clock__time--labels">Hours</li>
+    <li class="clock__time--labels">Minutes</li>
+    <li class="clock__time--labels">Seconds</li>
+  </ul>
 `;
 }
 
